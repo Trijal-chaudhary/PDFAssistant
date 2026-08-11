74f7a6a3122e090d982e628d0041e785f6603b0e
Harsh Vardhan Chaudhary <harshvardhanchaudhary86@gmail.com>
2026-08-11 19:17:27 +0530

    till now

---
+++ client/public/components/ChatWithPDF/ChatWithPDF.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import './ChatWithPDF.css'
+interface Message{
+  sender : "ai" | "user",
+  text : string
+}
+const ChatWithPDF = () => {
+  const [question, setQuestion] = useState("");
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      sender: "ai",
+      text: "Hello 👋 Ask me anything about your uploaded PDF."
+    }
+  ])
+
+  function handleSend() {
+
+    if (!question.trim()) return;
+
+    // Add user's question.
+    setMessages((prev) => [
+      ...prev,
+      {
+        sender: "user",
+        text: question
+      }
+    ]);
+
+    // Backend call will come here later.
+    // AI response will be appended after receiving it.
+
+    setQuestion("");
+  }
+  return (
+    <div className="chatPage4728">
+
+      <div className="header4728">
+
+        <div>
+
+          <h2>AI PDF Assistant</h2>
+
+          <p>📄 Express.js.pdf</p>
+
+        </div>
+
+        <button>
+          Upload New PDF
+        </button>
+
+      </div>
+
+      <div className="chatContainer4728">
+
+        {
+          messages.map((message, index) => (
+
+            <div
+              key={index}
+              className={
+                message.sender === "user"
+                  ? "message4728 user4728"
+                  : "message4728 ai4728"
+              }
+            >
+
+              {message.text}
+
+            </div>
+
+          ))
+        }
+
+      </div>
+
+      <div className="inputArea4728">
+
+        <input
+          type="text"
+          placeholder="Ask anything about your PDF..."
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+
+        <button onClick={handleSend}>
+          Send
+        </button>
+
+      </div>
+
+    </div>
+
+  )
+}
+
+export default ChatWithPDF
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import UploadPDF from '../public/components/UploadPDF/UploadPDF.tsx'
+import ChatWithPDF from '../public/components/ChatWithPDF/ChatWithPDF.tsx'
 function App() {
 
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element ={<UploadPDF/>}/>
+      <Route path='/chat_with_pdf' element ={<ChatWithPDF/>}/>
     </Routes>
     </BrowserRouter>
   )
